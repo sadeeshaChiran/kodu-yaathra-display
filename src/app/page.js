cdf79c6a56196b5067a3fb5f38025c7521bdcb4f
sadeeshaChiran <pmsckaushalya@gmail.com>
2025-08-24 14:18:25 +0530

    Remove unused files and components to clean up the project structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
-
-
+import './page.css'
+import './Invitation.css'
+import KoduYathra from "./KoduYathra"
 
 export default function Home() {
+
   return (
-    <>
-     <div>
-   
-      
+    <div className="root">
+
+      <KoduYathra />
+
     </div>
-    </>
-  );
+  )
 }
+
+
